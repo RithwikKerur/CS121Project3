867769fea9676f291ab5475e4d6ae18a643d3be7
RithwikKerur <72475174+RithwikKerur@users.noreply.github.com>
2022-11-10 17:53:14 -0800

    Added Indexing

--- indexer.py
+++ indexer.py
@@ -1,6 +1,7 @@
 import os 
 from collections import defaultdict
 import partA
+import json
 
 def index():
     directory = 'Dev'
@@ -12,35 +13,43 @@ def index():
     doc_count = 1
 
     for dirname in os.listdir(directory):
-        for filename in os.listdir(directory + '/' + dirname):
-            f = os.path.join(directory, dirname, filename)
-            all_tokens = partA.tokenize(f)
-            print(all_tokens)
-            if len(all_tokens) < 10000:
-                doc_index[doc_count] = filename
-                with open(f, 'r') as curr_file:
-                    url = curr_file.readline()
-                    defrag_url = url.split('#')[0]
-                    unique_pages.add(defrag_url.rstrip('\n'))
+        if os.path.isdir(directory + '/' + dirname):
+            for filename in os.listdir(directory + '/' + dirname):
+                f = os.path.join(directory, dirname, filename)
+                all_tokens = partA.tokenize(f)
+                if len(all_tokens) < 10000:
+                    doc_index[doc_count] = filename
+                    with open(f, 'r') as curr_file:
+                        url = curr_file.readline()
+                        defrag_url = url.split('#')[0]
+                        unique_pages.add(defrag_url.rstrip('\n'))
+                    
+                    words = partA.countFrequencies(all_tokens, words)
+                    for word, freq in words.items():
+                        if word in inverted_index:
+                            inverted_index[word][doc_count] = freq
+                        else:
+                            inverted_index[word] = dict()
+                            inverted_index[word][doc_count] = freq
+                    
+                    words = defaultdict(int)
 
-                # with open(filename, 'r') as f:
-                #print(f'{url} has {len(all_tokens)} number of words')
-                
-                words = partA.countFrequencies(all_tokens, words)
-                print(words)
-                for word in words:
-                    if word in inverted_index:
-                        inverted_index[word].append(doc_count)
-                    else:
-                        inverted_index[word] = [doc_count]
-                
-                words = defaultdict(int)
+                    doc_count += 1
+        
 
-                doc_count += 1
-        break
-    print(inverted_index)
-    print(doc_count)
+    j = json.dumps(inverted_index)
+    f = open("inverted_index.json", 'w')
+    f.write(j)
+    f.close()
+
+    j = json.dumps(doc_index)
+    f = open("doc_index.json", 'w')
+    f.write(j)
+    f.close()
     
 if __name__ == '__main__':
-    index()
+    #index()
+    with open('inverted_index.json', encoding='utf-8') as f:
+      data = json.load(f)
+      print(len(data))
                 
--- partA.py
+++ partA.py
@@ -1,6 +1,9 @@
 # python partA.py testfile.txt (How it will get called)
 from collections import defaultdict
 import sys
+import json 
+import io
+from bs4 import BeautifulSoup
 
 stopwords = set(['a',	'about',	'above',	'after',	'again',	'against',	'all',	'am',	'an',	'and',	'any',	'are','as',	'at',	'be',	'because',	'been',	'before',	'being',	'below',	'between',	'both',	'but',	'by',	'cannot',	'could',	'did',		'do',	'does',		'doing',	'down',	'during',	'each',	'few',	'for',	'from',	'further',	'had',	'has',		'have',		'having',	'he',	'her',	'here',		'hers',	'herself',	'him',	'himself',	'his',	'how',		'i','if',	'in',	'into',	'is',		'it',		'its',	'itself',		'me',	'more',	'most',		'my',	'myself',	'no',	'nor',	'not',	'of',	'off',	'on',	'once',	'only',	'or',	'other',	'ought',	'our',	'ours',	'out',	'over',	'own',	'same',		'she',		'should',		'so',	'some',	'such',	'than',	'that',		'the',	'their',	'theirs',	'them',	'themselves',	'then',	'there',		'these',	'they',		'this',	'those',	'through',	'to',	'too',	'under',	'until',	'up',	'very',	'was',	'we',			'were',		'what',	'when',	'where',	'which',	'while',	'who',	'whom',	'why',	'with',	'would',	'you','your',	'yours',	'yourself',	'yourselves',	'ourselves'])
 
@@ -14,7 +17,10 @@ def tokenize(file: str) -> list:
   #f'Downloads/{file}'
   try:
     with open(file, encoding='utf-8') as f:
-      f.readline()
+      data = json.load(f)
+      parser = BeautifulSoup(data['content'], 'xml')
+      print(data['encoding'])
+      f = io.StringIO(parser.get_text())
       if f.readable():
         while True:
           try:
@@ -33,6 +39,7 @@ def tokenize(file: str) -> list:
           except Exception as e:
             all_tokens.append("".join(curr_token).lower())
             curr_token = []
+      
   except Exception as e:
     print(e)
     exit()

abef10a9c9f528ab42e527db7dc4477b8a49fcdc
Avent Chiu <chiu7a@gmail.com>
2022-12-01 13:39:55 -0800

    Moved flask file around

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, redirect, url_for, request
+import query
 
 app = Flask(__name__)
 
 @app.route('/success/<name>')
 def success(name):
+    query.query(name)
     return 'welcome %s' % name
 
 @app.route('/search', methods=['POST', 'GET'])
-def login():
+def search():
     if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
+        search_query = request.form['nm']
+        return redirect(url_for('success', name=search_query))
     else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+        search_query = request.args.get('nm')
+        return redirect(url_for('success', name=search_query))
 
 if __name__ == '__main__':
     app.run(debug=True)
